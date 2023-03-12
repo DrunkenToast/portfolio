2c851856f41314109079dd60bdd71ea5f6a46a64
Peter Leconte <peter5leconte@gmail.com>
2023-03-12 11:26:04 +0100

    Update projects

--- src/static/cards.ts
+++ src/static/cards.ts
@@ -3,6 +3,13 @@ import { Tags } from "./tags";
 
 export const Cards: CardData[] = [
     {
+        title: 'Archived thoughts',
+        description: 'Astro blog theme for posts with tags with dark and light theme.',
+        tags: [Tags.astro],
+        link: 'https://github.com/DrunkenToast/portfolio',
+        pinned: false,
+    },
+    {
         title: 'Ruest client',
         description: 'Blazingly 🔥 fast Rust TUI client, developing it together with friends.',
         tags: [Tags.rust],
@@ -10,43 +17,57 @@ export const Cards: CardData[] = [
         pinned: false,
     },
     {
+        title: 'Crabsay - Cross development',
+        description: 'Tauri application for generating images of your favourite crabs. \
+        Comparing it to Electron, Qt and Ionic with capacitor.',
+        tags: [Tags.rust, Tags.tauri, Tags.ionic, Tags.electron, Tags.qt],
+        link: 'https://github.com/DrunkenToast/crabsay-tauri/blob/main/report/ReportCrossDev_p3_Peter_Leconte.md',
+        pinned: false,
+    },
+    {
         title: 'This portfolio',
         description: 'You\'re looking at it! Made with Vue.js and Vite.',
-        tags: [Tags.vue, Tags.vite, Tags.tailwind, Tags.typescript],
+        tags: [Tags.vue, Tags.tailwind, Tags.typescript],
         link: 'https://github.com/DrunkenToast/portfolio',
         pinned: false,
     },
     {
         title: 'Rust Arduino Project',
-        description: 'Rust multi-threaded API and Arduino communicate with serial. Arduino measures DHT and listens to actions.',
+        description: 'Rust multi-threaded API and Arduino communicate with \
+        serial. Arduino measures DHT and listens to actions.\
+        Brought together with a Next.js frontend.',
         tags: [Tags.rust, Tags.next, Tags.typescript, Tags.docker],
         link: 'https://github.com/DrunkenToast/arduino-project',
         pinned: true,
     },
     {
         title: 'Pillow',
-        description: 'Ambient sounds to relax to. Angular school project for GUI development.',
-        tags: [Tags.angular, Tags.material, Tags.typescript],
-        link: 'https://github.com/DrunkenToast/gui-project',
+        description: 'Ambient sounds to relax to.\
+        Angular school project for GUI development.',
+        tags: [Tags.angular, Tags.material, Tags.firebase, Tags.typescript],
+        link: 'https://pillow-c3b4c.web.app/',
         pinned: true,
     },
     {
         title: 'CLI Tirecenter',
-        description: 'CLI program to manage a tirecenter. School project about Object Oriented Programming in C++.',
-        tags: [Tags.cplusplus],
+        description: 'CLI program to manage a tirecenter.\
+        School project about Object Oriented Programming in C++.',
+        tags: [Tags.cpp],
         link: 'https://github.com/DrunkenToast/OOP_Tirecenter',
         pinned: false,
     },
     {
         title: 'Webshop',
-        description: 'Webshop with PHP and a MySQL database.',
+        description: 'Webshop with PHP, MySQL database and bootstrap.\
+        School project for web engineering.',
         tags: [Tags.php, Tags.sql],
         link: 'https://github.com/DrunkenToast/webshop-php-mysql',
         pinned: false,
     },
     {
         title: 'Jerma soundboard',
-        description: 'A soundboard Android app for everybody\'s favourite streamer written in Kotlin. School project for app development.',
+        description: 'A soundboard Android app for everybody\'s favourite \
+        streamer written in Kotlin. School project for app development.',
         tags: [Tags.kotlin],
         link: 'https://github.com/DrunkenToast/appdev',
         pinned: false,
@@ -54,7 +75,7 @@ export const Cards: CardData[] = [
     {
         title: 'Canvas Discord bot',
         description: 'Discord bot that integrates Canvas LMS with Discord.',
-        tags: [Tags.typescript, Tags.firestore],
+        tags: [Tags.typescript, Tags.firebase],
         link: 'https://github.com/practice-enterprise',
         pinned: false,
     },
--- src/static/tags.ts
+++ src/static/tags.ts
@@ -11,23 +11,39 @@ export const Tags = {
         name: 'Vue',
         color: '#41b783',
     },
-    vite: {
-        name: 'Vite',
-        color: '#ffc31b',
-    },
     angular: {
         name: 'Angular',
         color: '#e23137',
+    },
+    ionic: {
+        name: 'Ionic',
+        color: '#176bff',
     },
     next: {
         name: 'Next',
         color: '#000',
     },
+    astro: {
+        name: 'Astro',
+        color: '#ff7e33',
+    },
+    tauri: {
+        name: 'Tauri',
+        color: '#ffc131',
+    },
+    electron: {
+        name: 'Electron',
+        color: '#9feaf9',
+    },
+    qt: {
+        name: 'Qt',
+        color: '#41cd52',
+    },
     docker: {
         name: 'Docker',
         color: '#2394ec',
     },
-    cplusplus: {
+    cpp: {
         name: 'C++',
         color: '#00599d',
     },
@@ -43,8 +59,8 @@ export const Tags = {
         name: 'SQL',
         color: '#d96b11',
     },
-    firestore: {
-        name: 'Firestore',
+    firebase: {
+        name: 'Firebase',
         color: '#f6830d',
     },
     kotlin: {
